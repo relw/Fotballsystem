651792af13438818145f53ef7e5ddf78895c09f2
R <48986558+relw@users.noreply.github.com>
2021-09-23 18:43:59 +0200

    Update hentAlleSpillere.php

--- hentAlleSpillere.php
+++ hentAlleSpillere.php
@@ -3,24 +3,21 @@
     $username = "id17642902_fotballsystemdb";
     $password = "Klregj24jgfr3-";
     $dbname = "id17642902_fotballsystem";
+    // Koble til
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Sjekke tilkobling
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = 'SELECT * FROM Spillere ORDER BY antallMaal+0 DESC';
+    $resultat = $conn->query($sql);
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+    //Lager array for å sette inn alle spillere
+    $spillere=array();
 
-$sql = 'SELECT * FROM Spillere ORDER BY antallMaal+0 DESC';
-$resultat = $conn->query($sql);
-
-
-  
-  $spillere=array();
-  
-  while($rad = mysqli_fetch_assoc($resultat)) {
-    $spillere[]=$rad;
-  }
+    while($rad = mysqli_fetch_assoc($resultat)) {
+        $spillere[]=$rad;
+     }
 
     
     echo json_encode($spillere);
